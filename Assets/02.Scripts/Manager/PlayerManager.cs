d8587f39301670d49a62d64251dc80c0d54db51c
N0name4-SDH <soh0927@naver.com>
2025-03-12 17:04:31 +0900

    [ Feat ] 캐릭터 움직임 구현
    캐릭터의 wasd, 점프, 화면 움직임 구현

---
+++ Assets/02.Scripts/Manager/PlayerManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    private static PlayerManager _instance;
+    public static PlayerManager Instance 
+    {
+        get
+        {
+            if(_instance == null)
+            {
+                Debug.Log("객체 없음");
+            }
+            return _instance;
+        }
+    }
+    public Player Player
+    {
+        get { return _player; }
+        set { _player = value; }
+    }
+
+    private Player _player;
+
+
+    private void Awake()
+    {
+        if(_instance == null)
+        {
+            _instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
---
+++ Assets/02.Scripts/Player/PlayerCondition.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCondition : Condition, IDamageable
+{
+    Condition health;
+    Condition hunger;
+    Condition thirst;
+    Condition stamina;
+
+    private void Start()
+    {
+        if (health == null || hunger == null || thirst == null || stamina == null)
+        {
+            Debug.LogError("PlayerCondition: Condition 컴포넌트가 설정되지 않았습니다.");
+        }
+    }
+
+    /// <summary>
+    /// 사망 시 처리
+    /// </summary>
+    public void Die()
+    {
+        Debug.Log("플레이어가 사망했습니다.");
+        // TODO: 게임 오버 처리, 리스폰 기능 추가
+    }
+    /// <summary>
+    /// 캐릭터 힐
+    /// </summary>
+    /// <param name="value">힐량</param>
+    public void Heal(float value)
+    {
+        health.Add(value);
+    }
+    /// <summary>
+    /// 허기 완화
+    /// </summary>
+    /// <param name="value">허기 완화량</param>
+    public void Eat(float value)
+    {
+        hunger.Add(value);
+    }
+    /// <summary>
+    /// 갈증 완화
+    /// </summary>
+    /// <param name="value">갈증 완화량</param>
+    public void Drink(float value)
+    {
+        thirst.Add(value);
+    }
+
+
+    /// <summary>
+    /// 적으로부터 데미지를 받았을 시 처리
+    /// </summary>
+    /// <param name="damage"></param>
+    public void TakePhysicalDamage(int damage)
+    {
+        health.curVal -= damage;
+
+        if(health.curVal == 0)
+        {
+            Die();
+        }
+    }
+}
