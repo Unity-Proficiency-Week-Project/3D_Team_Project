8631aec0307417a1f2bdbd9481a264299a119b8c
kdh5790 <k_0106@naver.com>
2025-03-12 15:44:40 +0900

    [ Feat ] 건축에 사용할 벽 프리팹 생성

---
+++ Assets/02.Scripts/Architect/Util/MeshSlice.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+public class MeshSlice : MonoBehaviour
+{
+    [SerializeField] private Transform[] slicePivots;
+
+    void Start()
+    {
+
+        GameObject[] gameObjects = MeshCut.Cut(gameObject, slicePivots[0].position, Vector3.left, GetComponent<MeshRenderer>().material);
+
+        Destroy(gameObjects[1]);
+
+        gameObjects = MeshCut.Cut(gameObject, slicePivots[1].position, Vector3.right, GetComponent<MeshRenderer>().material);
+
+        Destroy(gameObjects[1]);
+    }
+
+    void Update()
+    {
+
+    }
+}
