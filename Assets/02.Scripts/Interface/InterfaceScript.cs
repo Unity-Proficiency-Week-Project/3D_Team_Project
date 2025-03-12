c52bbcb44305e0bd96cd2c04f6365544f0ef4c95
KJane0120 <rlawoals5755@gmail.com>
2025-03-12 14:54:55 +0900

    [Feat]인터페이스 스크립트 생성
    IDamageable 추가했습니다.

---
+++ Assets/02.Scripts/Interface/InterfaceScript.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public interface IDamageable
+{
+    void TakePhysicalDamage(int damage);
+}
+
+public class InterfaceScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
