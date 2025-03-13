e0ec05d99959a58a13f28d00c16dbad1b439df5f
KJane0120 <rlawoals5755@gmail.com>
2025-03-13 10:57:27 +0900

    [Feat]체력바가 플레이어 시야로 항상 향하도록 설정
    +기존 HealthBar에 붙여줬던 EnemyCondition.cs를 최상단으로 옮겨서 추가하고 Die 함수 수정했습니다.

--- Assets/02.Scripts/Enemy/EnemyCondition.cs
+++ Assets/02.Scripts/Enemy/EnemyCondition.cs
@@ -1,10 +1,23 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemyCondition : Condition
 {
     public ItemData[] dropOnDeath;  //죽었을 때 드롭 아이템
+    private Slider Healthbarslider;
+    public Camera _camera;
+
+    private void Start()
+    {
+        Healthbarslider = slider;
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 lookDirection = _camera.transform.forward;
+        lookDirection.y = 0; // 수직 방향 변화를 막아 체력바가 뒤집히는 걸 방지
+        Healthbarslider.transform.rotation = Quaternion.LookRotation(lookDirection);
+    }
 
     public void TakeDamage(float damage)
     {
@@ -24,6 +37,7 @@ public class EnemyCondition : Condition
             Instantiate(dropOnDeath[i].dropPrefab, transform.position + Vector3.up * 2, Quaternion.identity);
         
         }
-        Destroy(GetComponentInParent<EnemyAI>().gameObject); //최상위 부모 오브젝트가 사라지도록
+        //Destroy(GetComponentInParent<EnemyAI>().gameObject); //최상위 부모 오브젝트가 사라지도록
+        Destroy(gameObject);
     }
 }
