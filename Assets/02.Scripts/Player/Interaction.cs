43d9592e667b2cb493c4bb434625dce0e6d61d17
N0name4-SDH <soh0927@naver.com>
2025-03-13 16:09:04 +0900

    [Feat] 상호작용 기능 생성 완료
    상호작용으로 아이템을 인벤토리에 넣을 수 있도록 만들었습니다.

--- Assets/02.Scripts/Player/Interaction.cs
+++ Assets/02.Scripts/Player/Interaction.cs
@@ -7,9 +7,61 @@ public class Interaction : MonoBehaviour
     public float checkRate = 0.05f;
     private float lastCheckTime;
     public float maxCheckDistance;
-    public LayerMask layermask;
+    public LayerMask layerMask;
 
     public GameObject curInteractGameObj;
+    private IInteractable curInteractable;
 
+    public TextMeshProUGUI promptText;
+    public Camera camera;
+
+    private void Start()
+    {
+        camera = Camera.main;
+    }
+
+    public void Update()
+    {
+        if (Time.time - lastCheckTime > checkRate)
+        {
+            lastCheckTime = Time.time;
+
+            Ray ray = camera.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2));
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, maxCheckDistance, layerMask))
+            {
+                if (hit.collider.gameObject != curInteractGameObj)
+                {
+                    curInteractGameObj = hit.collider.gameObject;
+                    curInteractable = hit.collider.GetComponent<IInteractable>();
+                    SetPromptText();
+                }
+            }
+            else
+            {
+                curInteractGameObj = null;
+                curInteractable = null;
+                promptText.gameObject.SetActive(false);
+            }
+        }
+    }
+
+    private void SetPromptText()
+    {
+        promptText.gameObject.SetActive(true);
+        promptText.text = curInteractable.GetInteractPrompt();
+    }
+
+    public void OnInteractInput(InputAction.CallbackContext context)
+    {
+        if (context.phase == InputActionPhase.Started && curInteractable != null)
+        {
+            curInteractable.OnInteract();
+            curInteractGameObj = null;
+            curInteractable = null;
+            promptText.gameObject.SetActive(false);
+        }
+    }
 
 }
