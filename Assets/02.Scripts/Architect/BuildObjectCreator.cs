97970f667d650be77891cfe00994beb83a4b25f5
kdh5790 <k_0106@naver.com>
2025-03-13 12:22:31 +0900

    [ Feat ] 건축 프리뷰 오브젝트 정렬 기능 추가
    아직 회전 적용 X

--- Assets/02.Scripts/Architect/BuildObjectCreator.cs
+++ Assets/02.Scripts/Architect/BuildObjectCreator.cs
@@ -1,11 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+
+public enum PivotDirection
+{
+    Up,
+    Down,
+    Left,
+    Right
+}
 
 public class BuildObjectCreator : MonoBehaviour
 {
     [SerializeField] private GameObject buildPrefab;
     [SerializeField] private LayerMask buildableLayer;
+    [SerializeField] private List<Transform> previewObjPivots;
 
     private bool canBuild;
     private GameObject previewObj;
@@ -75,6 +85,11 @@ public class BuildObjectCreator : MonoBehaviour
 
         previewObj = Instantiate(obj);
 
+        // 프리뷰 오브젝트의 피벗들을 가져온 후 자신의 트랜스폼은 삭제
+        previewObjPivots = previewObj.GetComponentsInChildren<Transform>().ToList();
+
+        previewObjPivots.Remove(previewObjPivots.Find(x => x.name == previewObj.name));
+
         StartCoroutine(CanBuildRayCheck());
     }
 
@@ -99,51 +114,101 @@ public class BuildObjectCreator : MonoBehaviour
             yield break;
         }
 
+        // Ray에 자신이 감지되는걸 방지하기 위해 프리뷰 오브젝트의 레이어를 디폴트로 변경
         previewObj.layer = 0;
+
+        RaycastHit hitInfo;
 
         while (previewObj != null)
         {
-            Vector3 bottomPoint = renderer.bounds.center;
-            bottomPoint.y -= renderer.bounds.extents.y - 0.2f;
-
             if (CheckForObstacles(renderer))
             {
-                Debug.DrawRay(bottomPoint, Vector3.down, Color.red, 1f);
-                if (Physics.Raycast(bottomPoint, Vector3.down, out RaycastHit hitInfo, 1f, buildableLayer))
-                {
-                    if(hitInfo.collider.gameObject.layer == LayerMask.NameToLayer("BuildObject"))
-                    {
-                        Transform nearPivot = FindNearPivot(previewObj.transform.position, hitInfo.collider.gameObject);
+                // 피벗 위치별로 디버그용 Ray 생성
+                Debug.DrawRay(previewObjPivots[(int)PivotDirection.Up].position, Vector3.up, Color.red, 1f);
+                Debug.DrawRay(previewObjPivots[(int)PivotDirection.Down].position, Vector3.down, Color.red, 1f);
+                Debug.DrawRay(previewObjPivots[(int)PivotDirection.Left].position, Vector3.left, Color.red, 1f);
+                Debug.DrawRay(previewObjPivots[(int)PivotDirection.Right].position, Vector3.right, Color.red, 1f);
 
-                        if (nearPivot != null)
+                Vector3[] directions = new Vector3[]
+                {
+                    Vector3.up,
+                    Vector3.down,
+                    Vector3.left,
+                    Vector3.right,
+                };
+
+                // 피벗별로 Ray 검사
+                for (int i = 0; i < previewObjPivots.Count; i++)
+                {
+                    // 현재 피벗에서 정해둔 방향으로 Ray를 발사, buildableLayer에 포함되는 오브젝트가 충돌하면 hitInfo에 오브젝트 넣은 후 true 반환
+                    if (Physics.Raycast(previewObjPivots[i].position, directions[i], out hitInfo, 1f, buildableLayer))
+                    {
+                        // 오브젝트의 레이어가 BuildObject라면
+                        if (hitInfo.collider.gameObject.layer == LayerMask.NameToLayer("BuildObject"))
                         {
-                            previewObj.transform.position = nearPivot.position;
-                            previewObj.transform.rotation = nearPivot.rotation;
+                            // 프리뷰 오브젝트 기준 가장 근처에 있는 피벗을 찾음
+                            Transform nearPivot = FindNearPivot(previewObj.transform.position, hitInfo.collider.gameObject);
+
+                            if (nearPivot != null)
+                            {
+                                if (directions[i] == Vector3.up)
+                                {
+                                    Debug.Log($"{nearPivot.name} direction = up");
+                                    previewObj.transform.position = hitInfo.collider.transform.position + (Vector3.down * 2f);
+                                }
+
+                                else if (directions[i] == Vector3.down)
+                                {
+                                    Debug.Log($"{nearPivot.name} direction = down");
+                                    previewObj.transform.position = nearPivot.position;
+                                }
+
+                                else if (directions[i] == Vector3.left)
+                                {
+                                    previewObj.transform.position = hitInfo.collider.transform.position + (hitInfo.collider.transform.right * 2f);
+                                }
+
+                                else if (directions[i] == Vector3.right)
+                                {
+                                    previewObj.transform.position = hitInfo.collider.transform.position + (-hitInfo.collider.transform.right * 2f);
+
+                                }
+
+
+                                // 회전값을 같게 만들어줌
+                                previewObj.transform.rotation = nearPivot.rotation;
+
+                                // 프리뷰 오브젝트의 색을 초록색으로 변경하여 건설 가능 지점임을 알려줌
+                                canBuild = true;
+                                renderer.material.color = Color.green;
+
+                                break;
+                            }
+                        }
+                        // 오브젝트의 레이어가 Ground라면 y값을 충돌한 오브젝트와 맞춤
+                        else if (hitInfo.collider.gameObject.layer == LayerMask.NameToLayer("Ground"))
+                        {
+                            previewObj.transform.position = new Vector3(
+                                previewObj.transform.position.x,
+                                hitInfo.point.y,
+                                previewObj.transform.position.z);
 
                             canBuild = true;
                             renderer.material.color = Color.green;
 
-                            yield return null;
-                            continue;
+                            break;
                         }
                     }
-                    else if (hitInfo.collider.gameObject.layer == LayerMask.NameToLayer("Ground"))
+                    // Ray가 오브젝트와 충돌하지 않았을 경우
+                    else
                     {
-                        previewObj.transform.position = new Vector3(
-                            previewObj.transform.position.x,
-                            hitInfo.point.y,
-                            previewObj.transform.position.z);
-
-                        canBuild = true;
-                        renderer.material.color = Color.green;
+                        renderer.material.color = Color.red;
+                        canBuild = false;
                     }
                 }
-                else
-                {
-                    renderer.material.color = Color.red;
-                    canBuild = false;
-                }
             }
+
+            // 프리뷰 오브젝트 위치에 장애물이 있을 경우
             else
             {
                 renderer.material.color = Color.red;
@@ -160,12 +225,14 @@ public class BuildObjectCreator : MonoBehaviour
     /// <returns>주변에 오브젝트가 있다면 false, 없다면 true</returns>
     private bool CheckForObstacles(MeshRenderer renderer)
     {
+        // 메쉬 크기에 맞춰 OverapBox 검사
         Vector3 boxCenter = renderer.bounds.center;
 
         Vector3 boxSize = new Vector3(renderer.bounds.size.x, renderer.bounds.size.y, renderer.bounds.size.z);
 
         Collider[] colliders = Physics.OverlapBox(boxCenter, boxSize / 2.1f, Quaternion.identity);
 
+        // 박스에 충돌한 콜라이더의 레이어가 BuildObject인지 검사
         foreach (Collider collider in colliders)
         {
             if (collider.gameObject == previewObj) continue;
@@ -188,9 +255,11 @@ public class BuildObjectCreator : MonoBehaviour
         Transform nearPivot = null;
         float nearDistance = Mathf.Infinity;
 
-        // 모든 자식 중 "Pivot" 태그가 있는 Transform 탐색
+        // 모든 자식 중 Pivot 태그를 가지고 있는 오브젝트 중 가장 가까운 피벗 찾기
         foreach (Transform child in targetObject.transform)
         {
+            Debug.Log("find pivot");
+            
             if (child.CompareTag("Pivot"))
             {
                 float distance = Vector3.Distance(previewPosition, child.position);
@@ -214,7 +283,22 @@ public class BuildObjectCreator : MonoBehaviour
 
         GameObject go = Instantiate(previewObj);
         go.transform.position = previewObj.transform.position;
+
         go.GetComponent<MeshRenderer>().material.color = Color.white;
         go.layer = LayerMask.NameToLayer("BuildObject");
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (previewObj != null && previewObjPivots != null)
+        {
+            Gizmos.color = Color.blue;
+            Vector3 boxSize = new Vector3(1f, 1f, 1f); // OverlapBox 크기와 동일하게 설정
+
+            foreach (Transform pivot in previewObjPivots)
+            {
+                Gizmos.DrawWireCube(pivot.position, boxSize);
+            }
+        }
     }
 }
