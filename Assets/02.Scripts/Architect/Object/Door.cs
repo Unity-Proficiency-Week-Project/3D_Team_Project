e7745bcdf93c4097c0b2b06ecc081a438b6360f7
kdh5790 <k_0106@naver.com>
2025-03-14 21:46:05 +0900

    [ Feat ] 문 상호작용(열기/닫기) 추가
    벽에 붙어있는 문 열고 닫는 기능 추가

---
+++ Assets/02.Scripts/Architect/Object/Door.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Door : MonoBehaviour
+{
+    [SerializeField] Transform doorTransform;
+
+    private bool canInteraction;
+    private bool isOpen;
+
+    private Coroutine doorCoroutine;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.J) && canInteraction && doorCoroutine == null)
+        {
+            doorCoroutine = StartCoroutine(DoorInteraction());
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+            canInteraction = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+            canInteraction = false;
+    }
+
+    private IEnumerator DoorInteraction()
+    {
+        Vector3 startRotate = doorTransform.localRotation.eulerAngles;
+        Vector3 targetRotate = isOpen ? Vector3.zero : new Vector3(0, 90f, 0);
+        float duration = 0.5f;
+        float elapsedTime = 0f;
+
+        while (elapsedTime < duration)
+        {
+            Vector3 currentRot = Vector3.Lerp(startRotate, targetRotate, elapsedTime / duration);
+
+            doorTransform.localRotation = Quaternion.Euler(currentRot);
+
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        isOpen = !isOpen;
+        doorCoroutine = null;
+    }
+}
