81e3511dfb45a9290d438153aebd28136bcde75f
N0name4-SDH <soh0927@naver.com>
2025-03-17 21:24:13 +0900

    [ Feat ] 리소스 랜덤 배치 코드 작성
    리소스 랜덤 배치를 위한 코드를 작성하였습니다.

---
+++ Assets/02.Scripts/Spawner/ObjectSpawner.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSpawner : MonoBehaviour
+{
+    [Header("Spawn Settings")]
+    public GameObject[] enemyPrefabs;
+    public GameObject[] resourcePrefabs;
+
+    public int maxEnemy = 6;
+    public int maxResource = 15;
+
+    public Bounds spawnArea;
+    public LayerMask objectLayer;
+    public float minDistance = 3f;
+    public float respawnInterval = 3f;
+
+    private List<GameObject> activeEnemies = new List<GameObject>();
+    private List<GameObject> activeResources = new List<GameObject>();
+
+    private void Start()
+    {
+        SpawnObjects(enemyPrefabs, maxEnemy, activeEnemies);
+        SpawnObjects(resourcePrefabs, maxResource, activeResources);
+
+        StartCoroutine(CheckAndRespawn()); // 지속적으로 리스폰 체크
+    }
+
+    private void SpawnObjects(GameObject[] prefabs, int count, List<GameObject> activeList)
+    {
+        int maxAttempts = 10; // 최대 시도 횟수 (무한 루프 방지)
+
+        for (int i = 0; i < count; i++)
+        {
+            bool placed = false;
+            int attempts = 0;
+
+            while (!placed && attempts < maxAttempts)
+            {
+                Vector3 randomPosition = GetRandomPosition();
+
+                if (!IsPositionOccupied(randomPosition))
+                {
+                    GameObject prefab = prefabs[Random.Range(0, prefabs.Length)];
+                    GameObject spawnedObject = Instantiate(prefab, randomPosition, Quaternion.identity);
+                    activeList.Add(spawnedObject); // 리스트에 추가
+                    placed = true;
+                }
+                else
+                {
+                    attempts++;
+                }
+            }
+        }
+    }
+
+    private Vector3 GetRandomPosition()
+    {
+        float x = Random.Range(spawnArea.min.x, spawnArea.max.x);
+        float z = Random.Range(spawnArea.min.z, spawnArea.max.z);
+        float y = spawnArea.min.y; // 지형 높이를 기준으로 배치
+
+        return new Vector3(x, y, z);
+    }
+
+    private bool IsPositionOccupied(Vector3 position)
+    {
+        Collider[] colliders = Physics.OverlapSphere(position, minDistance, objectLayer);
+        return colliders.Length > 0;
+    }
+
+    private IEnumerator CheckAndRespawn()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(respawnInterval);
+
+            RespawnIfNeeded(enemyPrefabs, maxEnemy, activeEnemies);
+            RespawnIfNeeded(resourcePrefabs, maxResource, activeResources);
+        }
+    }
+
+    private void RespawnIfNeeded(GameObject[] prefabs, int maxCount, List<GameObject> activeList)
+    {
+        // 리스트에서 삭제된 오브젝트 정리
+        activeList.RemoveAll(item => item == null);
+
+        int missingCount = maxCount - activeList.Count;
+        if (missingCount > 0)
+        {
+            Debug.Log($"리스폰 진행: {missingCount}개 추가 생성 필요!");
+            SpawnObjects(prefabs, missingCount, activeList);
+        }
+    }
+
+
+}
