d9d1380c0678f03b73fa17e72d3193d6da7a9d1b
KJane0120 <rlawoals5755@gmail.com>
2025-03-17 22:56:14 +0900

    [Feat]드랍아이템 랜덤 출현
    적을 죽였을 때 나오는 드랍아이템이 랜덤하게 드랍될 수 있도록 했습니다.
    중복 허용합니다.

--- Assets/02.Scripts/Enemy/EnemyCondition.cs
+++ Assets/02.Scripts/Enemy/EnemyCondition.cs
@@ -1,21 +1,22 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class EnemyCondition : Condition
 {
-    public EnemyData data;  //죽었을 때 드롭 아이템
     private Slider Healthbarslider;
     private Camera _camera;
+    // 아이템 드랍 풀 (Inspector에서 할당)
+    [SerializeField] private List<ItemData> droppableItems;
 
-    
     public Action DamageFlash;
 
     private void Start()
     {
         Healthbarslider = slider;
         _camera = PlayerManager.Instance.Player.GetComponentInChildren<Camera>();
-        data = GetComponent<EnemyAI>().data;
     }
 
     private void LateUpdate()
@@ -38,11 +39,31 @@ public class EnemyCondition : Condition
     public void Die()
     {
         Debug.Log("적 사망!");
-        
-        for (int i = 0; i < data.dropOnDeath.Length; i++)
+
+        List<ItemData> dropList = RandomDropItem();
+
+        foreach (ItemData data in dropList)
         {
-            Instantiate(data.dropOnDeath[i], transform.position + Vector3.up * 2, Quaternion.identity);
+            if (data.dropPrefab != null)
+            {
+                Instantiate(data.dropPrefab, transform.position + Vector3.up * 2, Quaternion.identity); // 아이템 프리팹 생성
+            }
+            else Debug.Log(data.dropPrefab == null);
         }
+
         Destroy(gameObject);
+    }
+
+    List<ItemData> RandomDropItem()
+    {
+        int randomCount = UnityEngine.Random.Range(1, droppableItems.Count); // 1~최대 개수까지 랜덤 개수 선택
+        //중복허용 O 코드
+        List<ItemData> list = new List<ItemData>();
+        for (int i = 0; i < randomCount; i++)
+        {
+            ItemData item = droppableItems[UnityEngine.Random.Range(0, droppableItems.Count)]; // 중복 허용 랜덤 선택
+            list.Add(item);
+        }
+        return list;
     }
 }
