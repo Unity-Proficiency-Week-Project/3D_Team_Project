dacafe89809f8b8a9be9a95536a5103ca3cda19b
Gaon99 <shk9680@icloud.com>
2025-03-17 20:24:48 +0900

    [Fix] Error

--- Assets/02.Scripts/ScriptableObject/BuildingData/BuildingData.cs
+++ Assets/02.Scripts/ScriptableObject/BuildingData/BuildingData.cs
@@ -1,6 +1,6 @@
 using System.Collections.Generic;
 using UnityEngine;
-using static CraftingRecipe;
+using static CraftRecipe;
 
 [CreateAssetMenu(fileName = "Build Data", menuName = "New Build Data")]
 public class BuildingData : ScriptableObject
