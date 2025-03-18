c5a4fdb704017026a78941c84a4beeb9ba67e1ae
N0name4-SDH <soh0927@naver.com>
2025-03-18 09:56:22 +0900

    [Feat] 업적(퀘스트?) 시스템 기본 요소 설정
    업적의 기본 설정 및 내용 확립

---
+++ Assets/02.Scripts/Achieve/Achievement.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[System.Serializable]
+public class Achievement
+{
+    public string name;
+    public string description;
+    public bool isAccepted;
+    public bool isUnlocked;
+    public int goal;
+    public int curProgress;
+}
---
+++ Assets/02.Scripts/Manager/AchievementManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AchievementManager : MonoBehaviour
+{
+    public static AchievementManager Instance;
+
+    [Header("Achievement List")]
+    public List<Achievement> achievements = new List<Achievement>();
+
+    private void Awake()
+    {
+        if (Instance == null) Instance = this;
+        else Destroy(gameObject);
+    }
+
+    public void UpdateAchievements(string achievements, int amount)
+    {
+
+
+    }
+
+}
